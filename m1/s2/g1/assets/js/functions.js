8b5ffd0843083a046b79c51293505166d67b93fd
MarLizZo <marketto140@libero.it>
2023-04-03 16:31:23 +0200

    JS calculator

--- m1/s2/g1/assets/js/functions.js
+++ m1/s2/g1/assets/js/functions.js
@@ -1,3 +1,34 @@
-function menuClick() {
-    alert("Button Clicked!");
+var operation = "+";
+var str = "Il risultato è ";
+
+function operationChanged() {
+    var op = document.getElementById('operations');
+    var choice = op.options[op.selectedIndex].text;
+    if (choice == "Addizione") {
+        document.getElementById('js-op').innerHTML = "+";
+        operation = "+";
+    } else if (choice == "Sottrazione") {
+        document.getElementById('js-op').innerHTML = "-";
+        operation = "-";
+    } else if (choice == "Moltiplicazione") {
+        document.getElementById('js-op').innerHTML = "*";
+        operation = "*";
+    } else if (choice == "Divisione") {
+        document.getElementById('js-op').innerHTML = "/";
+        operation = "/";
+    }
+}
+
+function jsDoOperation() {
+    var el_1 = document.getElementById('user-input').value;
+    var el_2 = document.getElementById('user-input-2').value;
+    if (operation == "+") {
+        document.getElementById('js-result').innerHTML= str +  (Number(el_1) + Number(el_2));
+    } else if (operation == "-") {
+        document.getElementById('js-result').innerHTML= str +  (Number(el_1) - Number(el_2));
+    } else if (operation == "*") {
+        document.getElementById('js-result').innerHTML= str +  (Number(el_1) * Number(el_2));
+    } else if (operation == "/") {
+        document.getElementById('js-result').innerHTML= str +  (Number(el_1) / Number(el_2));
+    }
 }
