cc80636a3e6283f11d5b4ee086441a4a46116e97
MarLizZo <marketto140@libero.it>
2023-05-10 17:47:34 +0200

    cleanup code

--- m2/s3/g3/main.js
+++ m2/s3/g3/main.js
@@ -53,91 +53,26 @@ const buildCards = function () {
 
         btnAddIdCounter++;
         btnDisIdCounter++;
-
-        // let newDiv = document.createElement("div");
-        // newDiv.classList.add(
-        //     "card",
-        //     "px-0",
-        //     "d-flex",
-        //     "flex-column",
-        //     "align-items-center",
-        //     "justify-content-between",
-        //     "bg-dark",
-        //     "text-light",
-        //     "border-secondary",
-        //     "h-100"
-        // );
-
-        // let image = document.createElement("img");
-        // image.src = el.img;
-        // windowWidth > 992
-        //     ? image.classList.add("card-img-top", "w-75")
-        //     : image.classList.add("card-img-top", "w-50");
-        // newDiv.appendChild(image);
-
-        // let divInfo = document.createElement("div");
-        // divInfo.classList.add(
-        //     "d-flex",
-        //     "flex-column",
-        //     "w-100",
-        //     "align-items-center"
-        // );
-
-        // let title = document.createElement("h5");
-        // title.classList.add("card-title", "mt-3");
-        // title.innerText = el.title;
-        // divInfo.appendChild(title);
-
-        // let info = document.createElement("p");
-        // info.classList.add("card-text");
-        // info.innerText = "Price: " + el.price + "$";
-        // divInfo.appendChild(info);
-
-        // let btnAdd = document.createElement("button");
-        // btnAdd.classList.add(
-        //     "btn",
-        //     "btn-sm",
-        //     "btn-primary",
-        //     "border-0",
-        //     "mt-2",
-        //     "w-75"
-        // );
-        // btnAdd.innerText = "Add to Cart";
-        // divInfo.appendChild(btnAdd);
-
-        // let btnDc = document.createElement("button");
-        // btnDc.classList.add(
-        //     "btn",
-        //     "btn-sm",
-        //     "btn-danger",
-        //     "border-0",
-        //     "my-2",
-        //     "w-75"
-        // );
-        // btnDc.innerText = "Remove Book";
-        // divInfo.appendChild(btnDc);
-        // btnDc.addEventListener("click", function () {
-        //     let p_1 = this.parentElement;
-        //     let p_2 = p_1.parentElement;
-        //     discardFunc(p_2.parentElement);
-        // });
-
-        // newDiv.appendChild(divInfo);
-        // newCol.appendChild(newDiv);
     });
 };
 
-document.getElementById("btn-cart").addEventListener("click", function () {
-    cartBooks.forEach((el) => {
-        myUl = document.getElementById("list-cart");
-        let newLi = document.createElement("li");
-        newLi.classList.add("my-1");
-        newLi.innerHTML = `<span class="text-info fw-bold m-0">Book: </span><span class="m-0">${el.title}</span> <span class="m-0">||</span> <span class="m-0 text-info fw-bold"> Price:</span> <span class="m-0">${el.price}$</span>`;
-        myUl.appendChild(newLi);
-    });
-});
-
 window.onload = function () {
+    fetch(endpoint)
+        .then((res) => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                throw new Error("Something gone wrong..");
+            }
+        })
+        .then((data) => {
+            booksArray = data;
+            buildCards();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
     if (localStorage.getItem("booksInCart")) {
         let savedArr = JSON.parse(localStorage.getItem("booksInCart"));
         document.getElementById("cart-counter").innerText = savedArr.length;
@@ -145,6 +80,16 @@ window.onload = function () {
             cartBooks.push(el);
         });
     }
+
+    document.getElementById("btn-cart").addEventListener("click", function () {
+        cartBooks.forEach((el) => {
+            myUl = document.getElementById("list-cart");
+            let newLi = document.createElement("li");
+            newLi.classList.add("my-1");
+            newLi.innerHTML = `<span class="text-info fw-bold m-0">Book: </span><span class="m-0">${el.title}</span> <span class="m-0">||</span> <span class="m-0 text-info fw-bold"> Price:</span> <span class="m-0">${el.price}$</span>`;
+            myUl.appendChild(newLi);
+        });
+    });
 
     document.getElementById("del-cart").addEventListener("click", function () {
         cartBooks = [];
@@ -155,19 +100,3 @@ window.onload = function () {
         }
     });
 };
-
-fetch(endpoint)
-    .then((res) => {
-        if (res.ok) {
-            return res.json();
-        } else {
-            throw new Error("Something gone wrong..");
-        }
-    })
-    .then((data) => {
-        booksArray = data;
-        buildCards();
-    })
-    .catch((err) => {
-        console.log(err);
-    });
