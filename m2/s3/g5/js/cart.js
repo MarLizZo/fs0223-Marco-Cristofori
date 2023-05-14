08ecebe554681025e3b11783f95494186a7fc5d8
MarLizZo <marketto140@libero.it>
2023-05-14 14:21:01 +0200

    cart page done

--- m2/s3/g5/js/cart.js
+++ m2/s3/g5/js/cart.js
@@ -1,14 +1,87 @@
 const endpoint = "https://striveschool-api.herokuapp.com/api/product/";
 const authKey =
     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDVkZTI5Yjg4Zjc0MDAwMTQyODc0MzAiLCJpYXQiOjE2ODM4NzQ0NTksImV4cCI6MTY4NTA4NDA1OX0.ACkQXf6zVDzxCKSNDJczEXq6v5Muf3pxqueHx4Dajl0";
-const prodArr = [];
-const prodArrOut = [];
+let prodArr = [];
 
-const buildPage = function (flag) {
-    if (flag) {
-        //build
+const goToDetails = function (id) {
+    location.assign(`./details.html?id=${id}`);
+};
+
+const buildPage = function (prod = null) {
+    let row = document.getElementById("myRow");
+    if (prod) {
+        row.innerHTML += `<div
+        class="col-12 mt-3 d-flex flex-column flex-lg-row border-bottom border-2 border-black justify-content-between align-items-center">
+        <div class="d-flex flex-column">
+            <img class="mb-2 rounded-2" src="${prod.imageUrl}">
+            <h4>${prod.name}</h4>
+            <p>${prod.price}$</p>
+        </div>
+        <div class="d-flex flex-column">
+            <button class="btn btn-outline-warning px-3 mb-3" onclick="goToDetails('${prod._id}')">View Product Details</button>
+            <button class="btn btn-outline-warning px-3 mb-3 mb-lg-0" onclick="deleteItems('${prod._id}')">Remove from Cart</button>
+        </div>
+    </div>`;
     } else {
-        //no items
+        row.innerHTML = `
+            <div class="col-12 mt-4 d-flex justify-content-center">No Items in your cart..</div>
+            <div class="col-12 mt-4 d-flex justify-content-center"><a href="./index.html"><button class="btn btn-outline-warning">Back to Homepage</button></a></div>
+        `;
+    }
+    document.querySelector(".spinner-border").classList.add("d-none");
+};
+
+const fetchProducts = function (id) {
+    fetch(endpoint + id, {
+        headers: {
+            Authorization: authKey,
+        },
+    })
+        .then((res) => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                throw new Error(res.status);
+            }
+        })
+        .then((data) => {
+            buildPage(data);
+        })
+        .catch((err) => showErrorAlert(err.message));
+};
+
+const getStorage = async function () {
+    if (localStorage.getItem("your_cart")) {
+        let row = document.getElementById("myRow");
+        row.innerHTML = "";
+        prodArr = JSON.parse(localStorage.getItem("your_cart"));
+        document.getElementById("prod-count").innerText = prodArr.length;
+        prodArr.forEach((element) => {
+            fetchProducts(element.id);
+        });
+        let total = document.getElementById("cart-total");
+        total.innerText =
+            prodArr.reduce((prev, curr) => {
+                return prev + curr.price;
+            }, 0) + "$";
+    } else {
+        buildPage();
+    }
+};
+
+const deleteItems = function (id) {
+    document.querySelector(".spinner-border").classList.remove("d-none");
+    let jsoned = JSON.parse(localStorage.getItem("your_cart"));
+    let index = jsoned.findIndex((el) => el.id == id);
+    jsoned.splice(index, 1);
+    if (jsoned.length < 1) {
+        localStorage.removeItem("your_cart");
+        document.querySelector(".spinner-border").classList.add("d-none");
+        buildPage();
+        document.getElementById("cart-total").innerText = "0$";
+    } else {
+        localStorage.setItem("your_cart", JSON.stringify(jsoned));
+        getStorage();
     }
 };
 
@@ -33,39 +106,6 @@ const showErrorAlert = function (code) {
         alert.append(wrapper);
     };
     appendAlert();
-};
-
-const fetchProducts = function (id) {
-    fetch(endpoint + id, {
-        headers: {
-            Authorization: authKey,
-        },
-    })
-        .then((res) => {
-            if (res.ok) {
-                return res.json();
-            } else {
-                throw new Error(res.status);
-            }
-        })
-        .then((data) => {
-            prodArrOut.push(data);
-        })
-        .catch((err) => showErrorAlert(err.message));
-};
-
-const getStorage = function () {
-    if (localStorage.getItem("your_cart")) {
-        prodArr = JSON.parse(localStorage.getItem("your_cart"));
-        if (prodArr.length < 1) {
-            buildPage(0);
-        } else {
-            prodArr.forEach((el) => {
-                fetchProducts(el.id);
-            });
-            buildPage(1);
-        }
-    }
 };
 
 window.onload = function () {
