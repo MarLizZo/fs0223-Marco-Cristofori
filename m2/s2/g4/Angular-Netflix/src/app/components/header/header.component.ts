a3f7ffe34b6ef7aa82c97ac75714ee9e9b87cd35
MarLizZo <marketto140@libero.it>
2023-06-25 20:58:00 +0200

    media + navbar 80%

--- m2/s2/g4/Angular-Netflix/src/app/components/header/header.component.ts
+++ m2/s2/g4/Angular-Netflix/src/app/components/header/header.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-
+  constructor(private route: ActivatedRoute) {
+    // this.isHome();
+  }
 }
